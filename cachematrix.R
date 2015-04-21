5ba5c71f97af1ddc952373d32800df09c21d2654
Alexander Ledenev <alexander.ledenev@gmail.com>
2015-04-20 21:20:39 -0700

    Lexical Scoping: Implementation of caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,95 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+##
+## These pair of functions implement caching the inverse of a matrix
+## rather than computing it repeatedly
 
-## Write a short comment describing this function
+
+## This function creates a special "matrix" object
+## that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsol <- function(solution) m <<- solution
+        getsol <- function() m
+        list(set = set, get = get, setsol = setsol, getsol = getsol)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsol()
+        if(!is.null(m)) {
+            message("Getting cached matrix")
+            return(m)
+        }
+        matr <- x$get()
+        m <- solve(matr, ...)
+        x$setsol(m)
+        m
+}
+
+## Main function with a smoke test
+
+main <- function() {
+        matr <- matrix(rep(1,16), nrow = 4, ncol = 4)
+        matr[2,2] <- 3
+        matr[3,3] <- 0
+        matr[4,4] <- 2
+
+        inversed <- matrix(
+                c( 1.5, -0.5,  1.0, -1.0,
+                  -0.5,  0.5,  0.0,  0.0,
+                   1.0,  0.0, -1.0,  0.0,
+                  -1.0,  0.0,  0.0,  1.0),
+                nrow = 4, ncol = 4)
+
+            ## Create new cached object
+
+        matrix_obj <- makeCacheMatrix(matr)
+
+            ## Print original object
+
+        message("Original matrix object:")
+        pointer <- if (is.null(matrix_obj$getsol())) {"NULL"} else {"Not NULL !!!"}
+        message(paste("Cached matrix: ", pointer, sep = ""))
+        print(matrix_obj$get())
+
+            ## First access to object
+
+        matrix_1 <- cacheSolve(matrix_obj)
+
+            ## Validation of result
+
+        message("Inversed matrix:")
+        print(matrix_1)
+
+        if (all.equal(matrix_1, inversed))
+            message("Inversed matrix is OK")
+        else {
+            message("Bad inversed matrix. Expected result:")
+            print(inversed)
+            return(1)
+        }
+
+            ## Second access to object
+
+        matrix_2 <- cacheSolve(matrix_obj)
+
+            ## Validation of result
+
+        if (all.equal(matrix_1, matrix_2)) {
+            message("Cached matrix is OK")
+            0
+        }
+        else {
+            message("Bad cached matrix. Test failed")
+            print(matrix_2)
+            1
+        }
 }
